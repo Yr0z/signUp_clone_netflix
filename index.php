4c0ef69295deff3c64fe83cf8d300e760d859c91
Yr0z <y.cocheteau@gmail.com>
2021-04-01 16:12:51 +0200

    first commit of the project - starter documents

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Netflix</title>
+	<link rel="stylesheet" type="text/css" href="design/default.css">
+	<link rel="icon" type="image/png" href="../assets/favicon.png">
+</head>
+<body>
+
+	<?php include('src/header.php'); ?>
+	
+	<section>
+		<div id="login-body">
+
+				<?php if(isset($_SESSION['connect'])) { ?>
+
+					<h1>Bonjour !</h1>
+					<?php
+					if(isset($_GET['success'])){
+						echo'<div class="alert success">Vous êtes maintenant connecté.</div>';
+					} ?>
+					<p>Qu'allez-vous regarder aujourd'hui ?</p>
+					<small><a href="logout.php">Déconnexion</a></small>
+
+				<?php } else { ?>
+					<h1>S'identifier</h1>
+
+					<?php if(isset($_GET['error'])) {
+
+						if(isset($_GET['message'])) {
+							echo'<div class="alert error">'.htmlspecialchars($_GET['message']).'</div>';
+						}
+
+					} ?>
+
+					<form method="post" action="index.php">
+						<input type="email" name="email" placeholder="Votre adresse email" required />
+						<input type="password" name="password" placeholder="Mot de passe" required />
+						<button type="submit">S'identifier</button>
+						<label id="option"><input type="checkbox" name="auto" checked />Se souvenir de moi</label>
+					</form>
+				
+
+					<p class="grey">Première visite sur Netflix ? <a href="inscription.php">Inscrivez-vous</a>.</p>
+				<?php } ?>
+		</div>
+	</section>
+
+	<?php include('src/footer.php'); ?>
+</body>
+</html>
